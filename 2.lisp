238cacef3c3dcb2e71d56b52fd70e3047d2687ac
Magnus Morton <magnus.morton@ed.ac.uk>
2019-12-06 16:28:59 +0000

    nearly done

--- 2.lisp
+++ 2.lisp
@@ -8,4 +8,14 @@
 (let ((lst (mapcar 'parse-integer (uiop:split-string (read-input) :separator ","))))
   (loop for i from 0 to (length lst) by 4
        until (= 99 (nth i lst))
-       do (print (nth i lst))))
+     do
+       (let* ((val (nth i lst))
+             (pos1 (nth (+ i 1) lst))
+             (pos2 (nth (+ i 2) lst))
+             (pos3 (nth (+ i 3) lst))
+             (arg1 (nth pos1 lst))
+             (arg2 (nth pos2 lst)))
+         (cond ((= 1 val) (setf (nth pos3 lst) (+ arg1 arg2)))
+               ((= 2 val) (setf (nth pos3 lst) (* arg1 arg2))))))
+  lst)
+
