4c353fad32de2becba94566849953c48c6c063fa
Magnus Morton <magnusmorton@icloud.com>
2019-12-07 00:21:09 +0000

    2.2 done

--- 2.lisp
+++ 2.lisp
@@ -5,17 +5,33 @@
   (with-open-file (in "input")
     (read-line in)))
 
-(let ((lst (mapcar 'parse-integer (uiop:split-string (read-input) :separator ","))))
+(defun get-input ()
+  (mapcar 'parse-integer (uiop:split-string (read-input) :separator ",")))
+
+(defun intcode (lst noun verb)
+  (setf  (subseq lst 1 3) (list noun verb))
   (loop for i from 0 to (length lst) by 4
-       until (= 99 (nth i lst))
+     until (= 99 (nth i lst))
      do
        (let* ((val (nth i lst))
-             (pos1 (nth (+ i 1) lst))
-             (pos2 (nth (+ i 2) lst))
-             (pos3 (nth (+ i 3) lst))
-             (arg1 (nth pos1 lst))
-             (arg2 (nth pos2 lst)))
+              (pos1 (nth (+ i 1) lst))
+              (pos2 (nth (+ i 2) lst))
+              (pos3 (nth (+ i 3) lst))
+              (arg1 (nth pos1 lst))
+              (arg2 (nth pos2 lst)))
          (cond ((= 1 val) (setf (nth pos3 lst) (+ arg1 arg2)))
                ((= 2 val) (setf (nth pos3 lst) (* arg1 arg2))))))
-  lst)
+  (car lst))
+
+(intcode (get-input) 12 2)
+
+(let ((res 0))
+  (loop for i from 0 to 100
+     until (= res 19690720)
+     do
+       (loop for j from 0 to 100
+          until (= res 19690720)
+          do
+            (print (cons i j))
+            (setf res (intcode (get-input) i j)))))
 
